809ffd7982489c1c8a2d339d109ce8d5d67ed4a0
Zoe0426 <zoe820426@gmail.com>
2022-03-04 09:54:15 +0800

    update

---
+++ JavaScript/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".products").mouseover(function () { 
+        $(".products-name1").addClass("animate__slideInLeft");
+        $(".products-name2").addClass("animate__slideInLeft");
+        $(".products-name3").addClass("animate__slideInLeft");
+        $(".products-name4").addClass("animate__slideInLeft");
+    });
+    $(".products-title").mouseover(function () { 
+        $(".products-name1").removeClass("animate__slideInLeft");
+        $(".products-name2").removeClass("animate__slideInLeft");
+        $(".products-name3").removeClass("animate__slideInLeft");
+        $(".products-name4").removeClass("animate__slideInLeft");
+    });
+    $(".close-ad").click(function (e) { 
+        e.preventDefault();
+        $(".ad").hide();
+    });  
+});
